49578d11a109e40ea6f2bb2b7a2b04c5df1a2ba2
Denwiliusz27 <danwielgosz@gmail.com>
2023-11-19 13:26:55 +0100

    zad01: many activities on result

--- zad01-calculator/zad01-calculator/ViewController.swift
+++ zad01-calculator/zad01-calculator/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     var calculations: String = ""
     var operators: [Character] = ["+", "-", "x", "/", "(", ")", "."]
-    
+    var equalsClicked: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,95 +35,121 @@ class ViewController: UIViewController {
         calculatorStack.text = calculations
     }
     
-    func addToCalculatiorStack(number: String){
-        calculations = calculations + number
+    func addToCalculatiorStack(element: String){
+        if equalsClicked {
+            if operators.contains(element) {
+                calculations = result.text!
+                calculatorStack.text = calculations
+                result.text = ""
+            } else {
+                clearInput()
+            }
+            equalsClicked = false
+        }
+        
+//        if operators.contains(element) {
+//            if equalsClicked {
+//                calculations = result.text!
+//                calculatorStack.text = calculations
+//                result.text = ""
+//                equalsClicked = false
+//            }
+//        }
+        
+        calculations = calculations + element
         calculatorStack.text = calculations
     }
     
+    func canCalculate() -> Bool {
+        if calculations != "" && !operators.contains(calculations.last!) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+
     @IBAction func add(_ sender: Any) {
-        if !operators.contains(calculations.last!) {
-            addToCalculatiorStack(number: "+")
+        if canCalculate() {
+            addToCalculatiorStack(element: "+")
         }
     }
     
     @IBAction func substract(_ sender: Any) {
-        if !operators.contains(calculations.last!) {
-            addToCalculatiorStack(number: "-")
+        if canCalculate() {
+            addToCalculatiorStack(element: "-")
         }
     }
     
     @IBAction func multiple(_ sender: Any) {
-        if !operators.contains(calculations.last!) {
-            addToCalculatiorStack(number: "*")
+        if canCalculate() {
+            addToCalculatiorStack(element: "*")
         }
     }
     
     @IBAction func divide(_ sender: Any) {
-        if !operators.contains(calculations.last!) {
-            addToCalculatiorStack(number: "/")
+        if canCalculate() {
+            addToCalculatiorStack(element: "/")
         }
     }
     
     @IBAction func equals(_ sender: Any) {
-        if !operators.contains(calculations.last!) {
-            let tempResult = NSExpression(format: calculations).expressionValue(with: nil, context: nil) as! Double
-            
-            print(floor(tempResult))
-            print(tempResult)
+        if canCalculate() {
+            let tempResult = NSExpression(format:calculations).expressionValue(with: nil, context: nil) as! Double
             
             if floor(tempResult) == tempResult {
                 result.text = String(format: "%.0f", tempResult)
-                print("tutaj")
             } else {
-                print("here")
                 result.text = String(format: "%.2f", tempResult)
             }
+            
+            equalsClicked = true
         }
     }
     
     @IBAction func dot(_ sender: Any) {
-        if calculations.last!.isNumber {
-            addToCalculatiorStack(number: ".")
+        if canCalculate() {
+            addToCalculatiorStack(element: ".")
         }
     }
     
     @IBAction func zero(_ sender: Any) {
-        addToCalculatiorStack(number: "0")
+        addToCalculatiorStack(element: "0")
     }
     
     @IBAction func one(_ sender: Any) {
-        addToCalculatiorStack(number: "1")
+        addToCalculatiorStack(element: "1")
     }
     
     @IBAction func two(_ sender: Any) {
-        addToCalculatiorStack(number: "2")
+        addToCalculatiorStack(element: "2")
     }
     
     @IBAction func three(_ sender: Any) {
-        addToCalculatiorStack(number: "3")
+        addToCalculatiorStack(element: "3")
     }
     
     @IBAction func four(_ sender: Any) {
-        addToCalculatiorStack(number: "4")
+        addToCalculatiorStack(element: "4")
     }
     
     @IBAction func five(_ sender: Any) {
-        addToCalculatiorStack(number: "5")
+        addToCalculatiorStack(element: "5")
     }
     
     @IBAction func six(_ sender: Any) {
-        addToCalculatiorStack(number: "6")
+        addToCalculatiorStack(element: "6")
     }
     
     @IBAction func seven(_ sender: Any) {
-        addToCalculatiorStack(number: "7")
+        addToCalculatiorStack(element: "7")
     }
     
     @IBAction func eight(_ sender: Any) {
-        addToCalculatiorStack(number: "8")
+        addToCalculatiorStack(element: "8")
     }
     
     @IBAction func nine(_ sender: Any) {
-        addToCalculatiorStack(number: "9")
+        addToCalculatiorStack(element: "9")
     }
 }
